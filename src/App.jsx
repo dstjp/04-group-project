4c2f145f20ef13446411575da5d9139b22c92662
Dylano <167641317+dstjp@users.noreply.github.com>
2025-01-24 11:53:51 +0100

    fixed capitlization on context

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import { MoviesList } from "./Components/MoviesList/MoviesList";
 import { Routes, Route } from "react-router";
-import { MovieProvider } from "./Context/MovieContext";
+import { MovieProvider } from "./context/MovieContext";
 
 export const App = () => {
   return (
     //Navbar
-    <>
+    <>  
       <MovieProvider>
         <Routes>
           <Route index element={<MoviesList />} />
