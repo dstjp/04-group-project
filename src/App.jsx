6b5d4c08f29123abec11e8946dafa7e004d9a371
Dylano <noob.hyper@gmail.com>
2025-01-26 11:02:44 +0100

    i react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import "./App.css";
 import { MoviesList } from "./Components/MoviesList/MoviesList";
 import { Routes, Route } from "react-router";
 import { MovieProvider } from "./context/MovieContext";
+import Nav from "./Components/Nav/Nav";
 
 export const App = () => {
 
   return (
     <>  
+
+    <Nav />
+
       <MovieProvider>
         <Routes>
           <Route index element={<MoviesList />} />
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Nav from "../Nav/Nav";
+
+export default function Navbar() {
+    return <nav className ="nav">
+        <a href="" class="site-title">MDB</a>
+
+    </nav>
+}
