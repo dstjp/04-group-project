b890a6b023835cff4130bff49b05e7103a5a0ca6
Dylano <noob.hyper@gmail.com>
2025-01-28 14:49:18 +0100

    fix mess

--- src/Pages/FavoriteList/FavoriteList.jsx
+++ src/Pages/FavoriteList/FavoriteList.jsx
@@ -3,21 +3,3 @@ import AddFavorite from "./AddFavorite";
 import "./FavoriteList.css";
 
 export default function FavoriteList() {}
-
-return (
-	<div className="favorite-list-container">
-		<h1>Favorite List</h1>
-		<form onSubmit={handleSubmit}>
-			<input
-				type="text"
-				onChange={(e) => setMovies(e.target.value)}
-				value={movies}
-			/>
-			<button type="submit">Add</button>
-		</form>
-
-		{favorite.map((movies) => (
-			<AddFavorite movies={movies} />
-		))}
-	</div>
-);
