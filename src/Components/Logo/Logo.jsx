4ac67d07f63ae50b4028f0a139d9e8e86d01fc1c
jyy009 <153378401+jyy009@users.noreply.github.com>
2025-01-28 14:34:40 +0100

    Style navbar, add eye icon

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import LogoImageYellow from "../../assets/LogoIcons/logoYellow.svg"
-import LogoImageBlack from "../../assets/LogoIcons/logoBlack.svg"
-import "./Logo.css"
+import React from "react";
+import LogoImageYellow from "../../assets/LogoIcons/logoYellow.svg";
+import LogoImageBlack from "../../assets/LogoIcons/logoBlack.svg";
+import "./Logo.css";
 
 function LogoImageFunction() {
   return (
@@ -9,7 +9,7 @@ function LogoImageFunction() {
       <img src={LogoImageYellow} className="logo logo-yellow" alt="" />
       <img src={LogoImageBlack} className="logo logo-black" alt="" />
     </>
-  )
+  );
 }
 
 export default LogoImageFunction;
