1a9270684b1bfb07dbfaa5d53433a8a0d62a9ddc
Dylano <noob.hyper@gmail.com>
2025-02-04 14:28:16 +0100

    add searchicon to navbar in mobile view

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import "./Navbar.css";
 import FAVORITE_ICON from "../../assets//NavbarIcons/navbarStar.png";
 import HOME_ICON from "../../assets//NavbarIcons/navbarHome.png";
 import WATCH_ICON from "../../assets//NavbarIcons/navbarEye.png";
+import SEARCH_ICON from "../../assets/NavbarIcons/navbarSearch.svg";
 import BlackLogo from "../../assets/LogoIcons/logoBlack.svg";
 
 function Nav() {
@@ -31,6 +32,15 @@ function Nav() {
 								<img
 									className="nav-icon favorite-icon"
 									srcSet={FAVORITE_ICON}
+									alt=""
+								/>
+							</NavLink>
+						</li>
+						<li className="nav-link">
+							<NavLink to="/searchbar">
+								<img
+									className="nav-icon search-icon"
+									srcSet={SEARCH_ICON}
 									alt=""
 								/>
 							</NavLink>
---
+++ src/Components/SearchBar/ShowSearchBar.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+
+export const ShowSearchBar = () => {
+	const [show, setShow] = useState();
+};
